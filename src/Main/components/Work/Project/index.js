27c17a32e6de972f4d093327d727dbb2a654865a
kennansalisbury <kennansalisbury@gmail.com>
2020-05-23 13:15:11 -0500

    fixed scrammble text

--- src/Main/components/Work/Project/index.js
+++ src/Main/components/Work/Project/index.js
@@ -15,7 +15,7 @@ const Project = props => {
                     <p>{props.description}</p>
                     <a href={props.demoLink} target="_blank" rel='noreferrer noopener'>Live Demo </a>
                     <br/>
-                    <p className="demo-info"><small>Test User un: {props.demoUn} pw: {props.demoPw}</small></p>
+                    <p className="demo-info">{props.demoUn ? <small>Test User un: {props.demoUn} pw: {props.demoPw}</small> : ''}</p>
                     <a href={props.github} target="_blank" rel='noreferrer noopener'>Github Repo {props.github2 ? '- Client' : ''}</a>
                     {props.github2 ? <><br/> <a href={props.github2} target="_blank" rel='noreferrer noopener'>Github Repo - Server</a></> : '' }
                 </div>
